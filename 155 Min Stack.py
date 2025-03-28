20309114e7d1069f6bf14da8de1084fdd705ac62
Tisaghu <samasher9@hotmail.com>
2025-03-28 10:56:53 -0700

    Refactor MinStack implementation - cleaner and simpler code

--- 155 Min Stack.py
+++ 155 Min Stack.py
@@ -1,76 +1,41 @@
 class MinStack(object):
 
     def __init__(self):
-        #initialize stack
         self.stack = []
-
-        #initialize min stack
-        self.min_stack = []
-
-        #initialize the min value 
-        self.min = None
-        
+        self.minStack = []
 
     def push(self, val):
-        """
-        :type val: int
-        :rtype: None
-        """
-        #push the element on to the stack
         self.stack.append(val)
-
-        #update min value if necessary
-        if self.min == None:
-            self.min = val
-            self.min_stack.append(val)
-        elif val <= self.min:
-            self.min = val
-            self.min_stack.append(val)
+        
+        if not self.minStack or val <= self.minStack[-1]:
+            self.minStack.append(val)
+        else:
+            self.minStack.append(self.minStack[-1])
 
     def pop(self):
-        """
-        :rtype: None
-        """
-        #check if this element is the min element
-        if self.stack[-1] == self.min_stack[-1]:
-            self.min_stack.pop()
-            if len(self.min_stack) == 0:
-                self.min = None
-            else:
-                self.min = self.min_stack[-1]
-
-        #pop the element from the stack
-        self.stack.pop()
-        
+        if self.stack:
+            self.stack.pop()
+            self.minStack.pop() 
 
     def top(self):
-        """
-        :rtype: int
-        """
-        #return the top element of the stack
-        return self.stack[-1]
-        
+        return self.stack[-1] if self.stack else None
 
     def getMin(self):
-        """
-        :rtype: int
-        """
-        #return the min element
-        return self.min
-        
+        return self.minStack[-1] if self.minStack else None
 
+test = MinStack()
 
-# Your MinStack object will be instantiated and called as such:
-# obj = MinStack()
-# obj.push(val)
-# obj.pop()
-# param_3 = obj.top()
-# param_4 = obj.getMin()
+instructions = ["MinStack","push","push","push","push","getMin","pop","getMin","pop","getMin","pop","getMin"]
+inputs = [[],[2],[0],[3],[0],[],[],[],[],[],[],[]]
 
-obj = MinStack()
-obj.push(0)
-obj.push(1)
-obj.push(0)
-print(obj.getMin())
-obj.pop()
-print(obj.getMin())
+for i in range(0, len(instructions)):
+    if instructions[i] == "MinStack":
+        test = MinStack()
+    elif instructions[i] == "push":
+        test.push(inputs[i][0])
+    elif instructions[i] == "pop":
+        test.pop()
+    elif instructions[i] == "top":
+        print(test.top())
+    elif instructions[i] == "getMin":
+        print(test.getMin())
