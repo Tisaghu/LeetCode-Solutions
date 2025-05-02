bab967102801741e5b6bbef807c5380f4e725d8a
Tisaghu <samasher9@hotmail.com>
2025-05-02 12:05:13 -0700

    Add helper functions file for problems that require extra data structures.

---
+++ Helper_Functions.py
@@ -0,0 +1,28 @@
+
+#Array to linked list creator
+def arrayToLinkedList(arr):
+    if len(arr) == 0:
+        return None
+    
+    head = ListNode(arr[0])
+    current = head
+
+    for i in range(1, len(arr)):
+        current.next = ListNode(arr[i])
+        current = current.next
+
+    return head
+
+
+#  build a binary tree from a list
+def build_tree(values):
+    if not values:
+        return None
+    nodes = [TreeNode(val) if val is not None else None for val in values]
+    kids = nodes[::-1]
+    root = kids.pop()
+    for node in nodes:
+        if node:
+            if kids: node.left = kids.pop()
+            if kids: node.right = kids.pop()
+    return root
