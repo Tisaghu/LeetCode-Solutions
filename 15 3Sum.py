78bf003ec698c5834b8521ae84bab41d19ec9df9
Tisaghu <samasher9@hotmail.com>
2025-05-27 15:26:20 -0700

    Refactor 15 3Sum to use two-pointer approach for improved efficiency - still does not pass all test cases but very close

--- 15 3Sum.py
+++ 15 3Sum.py
@@ -4,30 +4,31 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        #need to find three numbers x,y and z such that x + y + z = 0
-        #if I fix one of those numbers, then I need to find two numbers that add up to the negative of that number
-        #I can use the two sum approach to find those two numbers
-        #I only need two pointers I think
-        #I can sort the array first to make it easier to find the two numbers
-
         #sort the array
         nums.sort()
         result = []
-        seen = []
 
         #it's like the 2sum problem with x being the target number
         for i in range(0,len(nums)-1):
             fixed = nums[i]
             target = -fixed
-            seen.append(fixed)
 
-            #now I need to find two numbers that add up to target that are not the fixed number
-            for j in range(0,len(nums)-1):
-                if j == i:
-                    continue
-                diff = target - nums[j]
-                if diff in seen:
-                    triplet = [fixed, diff, nums[j]]
+            #Since the array is sorted, can use a two pointer approach to efficiently find j and k
+            j, k = i + 1, len(nums)-1
+            while j < k:
+                currentSum = nums[j] + nums[k]
+                if currentSum == target:
+                    result.append([nums[i], nums[j], nums[k]])
+                    j += 1
+                    k -= 1
+                if currentSum > target:
+                    k -= 1
+                else:
+                    j += 1
+
+        return result
+
+
 
 
 
@@ -37,7 +38,7 @@ class Solution(object):
 
 
 test = Solution()
+print(test.threeSum([0,0,0,0]))
 print(test.threeSum([-1,0,1,2,-1,-4])) #[[-1,-1,2],[-1,0,1]]
-print(test.threeSum([0,1,1])) #[]
-print(test.threeSum([])) #[]
+
 
