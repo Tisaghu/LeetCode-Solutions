faf2b91618994c62e872269300939ded679d36cc
Tisaghu <samasher9@hotmail.com>
2025-06-18 12:09:03 -0700

    Add pytest test cases for #22, and change file naming convention.

---
+++ test_Generate_Parentheses_22.py
@@ -0,0 +1,14 @@
+import pytest
+from Generate_Parentheses_22 import Solution
+
+test_cases = [
+    (3, ["((()))", "(()())", "(())()", "()(())", "()()()"]),
+    (1, ["()"]),
+    (2, ["()()", "(())"])
+]
+
+@pytest.mark.parametrize("n, expected", test_cases)
+def test_generate_parenthesis(n,expected):
+    solver = Solution()
+    result = solver.generateParenthesis(n)
+    assert set(result) == set(expected)
