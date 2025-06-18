1da3b996da241c34a90946207630c203c867196a
Tisaghu <samasher9@hotmail.com>
2025-06-18 12:16:03 -0700

    Add initial implementation and tests for Koko Eating Bananas problem

---
+++ Koko_Eating_Bananas_875.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def minEatingSpeed(self, piles, h):
+        """
+        :type piles: List[int]
+        :type h: int
+        :rtype: int
+        """
+        
+        
+
+
+test = Solution()
+piles = [3,6,7,11]
+h = 8
+
+print(test.minEatingSpeed(piles,h))
---
+++ test_Koko_Eating_Bananas_875.py
@@ -0,0 +1,14 @@
+import pytest
+from Koko_Eating_Bananas_875 import Solution
+
+test_cases = [
+    ([3,6,7,11], 8, 4),
+    ([30,11,23,4,20], 5, 30),
+    ([30,11,23,4,20], 6, 23)
+]
+
+@pytest.mark.parametrize("piles, h, expected", test_cases)
+def test_minEatingSpeed(piles, h, expected):
+    solver = Solution()
+    result = solver.minEatingSpeed(piles, h)
+    assert set(result) == set(expected)
