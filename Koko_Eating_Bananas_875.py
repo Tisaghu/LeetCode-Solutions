b57fbaae4fb19b17c407315ffdf56b5ff2f31e83
Tisaghu <samasher9@hotmail.com>
2025-06-25 14:36:06 -0700

    Add solution and test cases for 875

--- Koko_Eating_Bananas_875.py
+++ Koko_Eating_Bananas_875.py
@@ -1,3 +1,5 @@
+import math
+
 class Solution(object):
     def minEatingSpeed(self, piles, h):
         """
@@ -5,12 +7,22 @@ class Solution(object):
         :type h: int
         :rtype: int
         """
+        #maximum value of k is the value of the largest pile
+        lower_bound, upper_bound = 1, max(piles)
+        res = upper_bound
+
+        #check values of k between 1 and max(piles) using binary search
+        while lower_bound <= upper_bound:
+            k = (lower_bound + upper_bound) // 2
+
+            totalTime = 0
+            for pile in piles:
+                totalTime += math.ceil(float(pile) / k)
+
+            if totalTime <= h:
+                res = k
+                upper_bound = k - 1
+            else:
+                lower_bound = k + 1
+        return res
         
-        
-
-
-test = Solution()
-piles = [3,6,7,11]
-h = 8
-
-print(test.minEatingSpeed(piles,h))
--- test_Koko_Eating_Bananas_875.py
+++ test_Koko_Eating_Bananas_875.py
@@ -4,11 +4,12 @@ from Koko_Eating_Bananas_875 import Solution
 test_cases = [
     ([3,6,7,11], 8, 4),
     ([30,11,23,4,20], 5, 30),
-    ([30,11,23,4,20], 6, 23)
+    ([30,11,23,4,20], 6, 23),
+    ([25,10,23,4], 4, 25)
 ]
 
 @pytest.mark.parametrize("piles, h, expected", test_cases)
 def test_minEatingSpeed(piles, h, expected):
     solver = Solution()
     result = solver.minEatingSpeed(piles, h)
-    assert set(result) == set(expected)
+    assert result == expected
