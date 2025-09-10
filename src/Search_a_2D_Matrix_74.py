658054800d4de3a9ddb198e76768c41bb8c17d88
Tisaghu <samasher9@hotmail.com>
2025-09-10 11:32:11 -0700

    Add solution for 74 - Search a 2D Matrix

--- src/Search_a_2D_Matrix_74.py
+++ src/Search_a_2D_Matrix_74.py
@@ -1,5 +1,3 @@
-
-
 class Solution(object):
     def searchMatrix(self, matrix, target):
         #first do a binary search on the rows to find out which one it's in
@@ -7,23 +5,32 @@ class Solution(object):
         rowLength = len(matrix[0])-1
         targetRow = None
 
-        while low != high:
+        # Handle edge case 
+        if target > matrix[high][rowLength] or target < matrix[low][0]:
+            return False
+
+        while low <= high:
             mid = (low + high) // 2
-            if matrix[mid][0] < target < matrix[mid][rowLength]:
-                targetRow = mid
+            if matrix[mid][0] <= target <= matrix[mid][rowLength]:
+                targetRow = matrix[mid]
                 break
             elif target > matrix[mid][rowLength]:
                 low = mid + 1
             elif target < matrix[mid][0]:
                 high = mid - 1
+        
+        if targetRow == None:
+            return False
 
-        print(targetRow)
+        # Do a second binary search in the target row
+        l, r = 0, rowLength
 
-
-test = Solution()
-test.searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 13)
-test.searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 3)
-
-
-
-
+        while l <= r:
+            mid = (l+r) // 2
+            if targetRow[mid] == target:
+                return True
+            elif target > targetRow[mid]:
+                l = mid + 1
+            else:
+                r = mid - 1
+        return False
--- tests/Search_a_2D_Matrix_74_test.py
+++ tests/Search_a_2D_Matrix_74_test.py
@@ -5,7 +5,10 @@ import pytest
 
 test_cases = [
     ([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 3, True),
-    ([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 13, False)
+    ([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 13, False),
+    ([[1]], 1, True),
+    ([[1]], 0, False),
+    ([[1],[3]], 2, False)
 ]
 
 @pytest.mark.parametrize("matrix, target, expected", test_cases)
